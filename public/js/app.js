9a89fe9dee1a8048e83c534673fc17db387124c0
Robert Casanova <robb.casanova@gmail.com>
2013-12-11 21:21:40 +0100

    Added recycool tab

--- public/js/app.js
+++ public/js/app.js
@@ -31,21 +31,62 @@ jQuery(function($){
 	RECYCOOL.animateDial = function(classe, arrivo){
 
 		$(classe).knob({
-			readOnly: true,
-			bgColor: '#fff',
-			fgColor: '#f8c114'
+			draw : function () {
+
+                    // "tron" case
+                    if(this.$.data('skin') == 'tron') {
+
+                        var a = this.angle(this.cv)  // Angle
+                            , sa = this.startAngle          // Previous start angle
+                            , sat = this.startAngle         // Start angle
+                            , ea                            // Previous end angle
+                            , eat = sat + a                 // End angle
+                            , r = true;
+
+                        this.g.lineWidth = this.lineWidth;
+
+                        this.o.cursor
+                            && (sat = eat - 0.3)
+                            && (eat = eat + 0.3);
+
+                        if (this.o.displayPrevious) {
+                            ea = this.startAngle + this.angle(this.value);
+                            this.o.cursor
+                                && (sa = ea - 0.3)
+                                && (ea = ea + 0.3);
+                            this.g.beginPath();
+                            this.g.strokeStyle = this.previousColor;
+                            this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sa, ea, false);
+                            this.g.stroke();
+                        }
+
+                        this.g.beginPath();
+                        this.g.strokeStyle = r ? this.o.fgColor : this.fgColor ;
+                        this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sat, eat, false);
+                        this.g.stroke();
+
+                        this.g.lineWidth = 2;
+                        this.g.beginPath();
+                        this.g.strokeStyle = this.o.fgColor;
+                        this.g.arc(this.xy, this.xy, this.radius - this.lineWidth + 1 + this.lineWidth * 2 / 3, 0, 2 * Math.PI, false);
+                        this.g.stroke();
+
+                        return false;
+                    }
+            }
 		});
 		
 		var knob = classe;
 		var arrivo = arrivo;
-		$({value: 0}).delay(300).animate({value: arrivo}, {
+		$({value: 0}).delay(300).animate({value: 30}, {
 			duration: 1000,
 			easing:'swing',
 			step: function() {
 				$(knob).val(Math.ceil(this.value)).trigger('change');
 				var valore = $(knob).val();
 				$(knob).val(valore+'%');
-			}
+			},
+			
 		});
 	};
 
